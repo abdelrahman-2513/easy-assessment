086bacadb1c7f5bf731e0592b80b858dc40cf752
Abdelrahman Hassan <ab19622001@gmail.com>
2025-05-13 12:10:40 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,12 +10,12 @@ async function bootstrap() {
   // Allow CORS for my frontend only
   app.use(helmet());
   app.enableCors({
-    origin: true, 
-    methods: 'GET,POST,PATCH,DELETE', 
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+      origin: ["http://localhost:5173", "https://easy-assessment-phi.vercel.app"],
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+      credentials: true,
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
